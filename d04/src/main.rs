9fa5db4d64f431704373a5e661d296612a04416a
Ben Gale <bengale2007@googlemail.com>
2023-12-04 12:59:36 +0000

    Clippy lints and used iterator

--- d04/src/main.rs
+++ d04/src/main.rs
@@ -14,9 +14,9 @@ fn main() {
     }
 }
 
-fn number_of_matches(numbers: Vec<&str>) -> usize {
-    let winning_numbers = numbers.first().unwrap();
-    let our_numbers = numbers.last().unwrap();
+fn number_of_matches(numbers: &[&str]) -> usize {
+    let winning_numbers = &numbers.first().unwrap();
+    let our_numbers = &numbers.last().unwrap();
 
     let mut winning_numbers: HashSet<_> = winning_numbers.split(' ').collect();
     winning_numbers.remove(&"");
@@ -32,20 +32,13 @@ fn number_of_matches(numbers: Vec<&str>) -> usize {
 fn scratchcards_value(input: impl AsRef<Path>) -> u64 {
     let lines = puzzle_input_lines(input);
 
-    let mut total = 0;
-    for line in lines {
-        let line = line.unwrap();
-
-        let numbers: Vec<_> = line.split(':').last().unwrap().split('|').collect();
-
-        let matches = number_of_matches(numbers);
-
-        if matches != 0 {
-            total += u64::pow(2, (matches - 1) as u32);
-        }
-    }
-
-    total
+    lines
+        .into_iter()
+        .map(Result::unwrap)
+        .map(|l| number_of_matches(&l.split(':').last().unwrap().split('|').collect::<Vec<_>>()))
+        .filter(|m| *m != 0)
+        .map(|m| u64::pow(2, u32::try_from(m - 1).unwrap()))
+        .sum()
 }
 
 fn total_scratchcards(input: impl AsRef<Path>) -> u64 {
@@ -58,12 +51,12 @@ fn total_scratchcards(input: impl AsRef<Path>) -> u64 {
 
     for (card, line) in lines.iter().enumerate() {
         let numbers: Vec<_> = line.split(':').last().unwrap().split('|').collect();
-        let matches = number_of_matches(numbers);
+        let matches = number_of_matches(&numbers);
         let current_card_count = { *card_counts.get(card).unwrap() };
 
         for i in card + 1..=card + matches {
             if let Some(c) = card_counts.get_mut(i) {
-                *c += current_card_count
+                *c += current_card_count;
             }
         }
     }
