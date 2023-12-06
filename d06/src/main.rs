fef5e5a85c87d17e022029697575b993fd368fa0
Ben Gale <bengale2007@googlemail.com>
2023-12-06 08:17:12 +0000

    Day 5 part 2
    Brute forcing

--- d06/src/main.rs
+++ d06/src/main.rs
@@ -5,12 +5,12 @@ use aoc_utils::{get_entire_puzzle, Cli};
 fn main() {
     let part_two = Cli::parse_args().part_two;
 
-    if part_two {
-        todo!()
+    let result = if part_two {
+        race_records_part2("input")
     } else {
-        let result = race_records("input");
-        println!("Race record product: {result}");
-    }
+        race_records("input")
+    };
+    println!("Race record product: {result}");
 }
 
 fn parse_line(puzzle_line: &str) -> Vec<u64> {
@@ -21,6 +21,17 @@ fn parse_line(puzzle_line: &str) -> Vec<u64> {
         .split_ascii_whitespace()
         .map(|n| n.parse().unwrap())
         .collect()
+}
+
+fn parse_line_part2(puzzle_line: &str) -> u64 {
+    let number_fragments: Vec<&str> = puzzle_line
+        .split(':')
+        .last()
+        .unwrap()
+        .split_ascii_whitespace()
+        .collect();
+
+    number_fragments.join("").parse().unwrap()
 }
 
 fn number_winning_times(time: u64, distance: u64) -> u64 {
@@ -42,6 +53,15 @@ fn race_records(input: impl AsRef<Path>) -> u64 {
         .product()
 }
 
+fn race_records_part2(input: impl AsRef<Path>) -> u64 {
+    let puzzle = get_entire_puzzle(input);
+
+    let time = parse_line_part2(puzzle.first().unwrap());
+    let distance = parse_line_part2(puzzle.get(1).unwrap());
+
+    number_winning_times(time, distance)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,5 +70,11 @@ mod tests {
     fn part_one() {
         let result = race_records("test_part1");
         assert_eq!(result, 288);
+    }
+
+    #[test]
+    fn part_two() {
+        let result = race_records_part2("test_part1");
+        assert_eq!(result, 71503);
     }
 }
