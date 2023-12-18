7d681340a2d72cd06fe002e6650fee9f7a329388
Ben Gale <bengale2007@googlemail.com>
2023-12-18 08:27:24 +0000

    Day 18 part 2

--- d18/src/main.rs
+++ d18/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let part_two = Cli::parse_args().part_two;
 
     let result = if part_two {
-        todo!()
+        lava_volume_hex("input")
     } else {
         lava_volume("input")
     };
@@ -28,6 +28,27 @@ fn lava_volume(input: impl AsRef<Path>) -> isize {
     // Pick's theorem
     let boundary_length: isize = dig_plan.iter().map(|r| r.distance).sum();
 
+    let internal_points = area + 1 - (boundary_length / 2);
+
+    boundary_length + internal_points
+}
+
+fn lava_volume_hex(input: impl AsRef<Path>) -> isize {
+    let dig_plan = parse_puzzle(input);
+    let mut coordinates: Vec<Coord> = vec![];
+    let mut current_position: Coord = (0, 0);
+    let mut boundary_length = 0;
+    for record in &dig_plan {
+        let (direction, distance) = record.hex_instruction();
+        for _ in 0..distance {
+            current_position = direction.next_coord(&current_position);
+        }
+        boundary_length += distance;
+        coordinates.push(current_position);
+    }
+    let area = shoelace_formula(&coordinates);
+
+    // Pick's theorem
     let internal_points = area + 1 - (boundary_length / 2);
 
     boundary_length + internal_points
@@ -72,6 +93,26 @@ impl FromStr for Record {
     }
 }
 
+impl Record {
+    fn hex_instruction(&self) -> (Direction, isize) {
+        let mut colour = self.colour.trim_matches(&['(', ')']).to_string();
+        let last_char = colour.pop().unwrap();
+
+        let colour = colour.strip_prefix('#').unwrap();
+        let distance = isize::from_str_radix(colour, 16).unwrap();
+
+        let direction = match last_char {
+            '0' => Direction::Right,
+            '1' => Direction::Down,
+            '2' => Direction::Left,
+            '3' => Direction::Up,
+            _ => panic!("Unrecognised pattern"),
+        };
+
+        (direction, distance)
+    }
+}
+
 fn parse_puzzle(input: impl AsRef<Path>) -> Vec<Record> {
     puzzle_input_lines(input)
         .map(Result::unwrap)
@@ -88,5 +129,11 @@ mod tests {
     fn part_one() {
         let result = lava_volume("test_part1");
         assert_eq!(result, 62);
+    }
+
+    #[test]
+    fn part_two() {
+        let result = lava_volume_hex("test_part1");
+        assert_eq!(result, 952408144115);
     }
 }
